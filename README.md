587db4dc8956c45afe3b22049df8429014dc0c19
careymp1017 <careymp1017@outlook.com>
2019-09-15 21:46:37 -0600

    I think I actually got it!  Finallygit add -A

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 The Psychic Game...my first struggle...err...foray  into Javascript.
 
+9/15/2019 - I think I actually did it!!!!!!!!!!
+
 Mike Carey
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,59 @@
 var wins = 0;
 var losses = 0;
-var left = 9;
-var guesses = [];
-var getLetter;
+var guessesLeft = 9;
+var guessesMade = [];
+var compLetter;
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"];
 
-var compLetter = function () {
-    getLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+// tells the browser to pick a letter from from alphabet[]
+function compGuess() {
+    compLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
     }
-compLetter()
-console.log(getLetter);
+compGuess();
+console.log(compLetter);
 
+// sets a function to start a new game
+function newGame() {
+    console.clear();
+    compGuess();
+    console.log(compLetter);
+    guessesMade = [];
+    guessesLeft = 9;
+    }
 
+document.onkeyup = function(event) {
+
+    // identifies the key pressed and assigns it to a variable
+    var userGuess = event.key;
+    console.log(userGuess); 
+    
+    // reduces the numebr of guesses left
+    guessesLeft--;
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+    // console.log(guessesLeft); 
+
+    // tracks each guess by user
+    guessesMade.push(userGuess);
+    console.log(guessesMade); 
+
+    // writes the user guesses to the HTML page
+    function trackGuesses() {
+        var writeGuesses = guessesMade.toString();
+        document.getElementById("guessesMade").innerHTML = writeGuesses;
+    }
+    trackGuesses();
+
+    if (guessesLeft > 0) {
+        if (userGuess == compLetter) {
+            wins++;
+            document.getElementById("wins").innerHTML = wins;
+            newGame();
+        }
+    }
+        else if ( guessesLeft == 0) {
+            losses++;
+            document.getElementById("losses").innerHTML = losses;
+            newGame();
+        }
+}
